22dad5473759e64d735d3beff21532b1468b374e
ed-deloboy <edone.olimp@yandex.ru>
2022-04-27 18:00:19 +0300

    Added img from server and load prelad

--- filter.php
+++ filter.php
@@ -1,5 +1,9 @@
 <?php
 
+// нужно получить выбранное id с селекта - filterValue, проверить в базе совпадение на data-category и вернуть совпадения, перебрать и отдать на фронт
+
+
 $filterValue = $_POST['filterValue'];
+$fullArr = $_POST['fullArr'];
 
 echo $filterValue;
